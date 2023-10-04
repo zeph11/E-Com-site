ef5c3b913bbc29a8b3b4a6c0d1989420a4bdb877
zeph111 <luniva121@gmail.com>
2023-10-04 21:25:09 +0545

    feat: completion of the initial draft

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,3 +1,4 @@
+import { copyrightSign } from "../assets/icons";
 import { footerLogo } from "../assets/images";
 import { footerLinks, socialMedia } from "../constants";
 
@@ -33,9 +34,12 @@ const Footer = () => {
       </div>
 
       <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm: items-center">
-        <div className="flex flex-1 justify start ">
-          <img/>
+        <div className="flex flex-1 justify start items-center gap-2 font-montserrat">
+          <img src={copyrightSign}
+          width={20} height={20} className="rounded-full m-0"/>
+          <p> Copyright. All rights reserved</p>
         </div>
+        <div> Terms and Condition</div>
       </div>
     </footer>
   );
